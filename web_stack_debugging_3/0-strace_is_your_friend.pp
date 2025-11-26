018dfc9e80323d864214a4d160c1c67f52d5cdc2
Abdull-Kudus <a.zakariam@alustudent.com>
2025-11-26 11:57:06 -0800

    refactor web3 update

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,24 +1,6 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file wp-settings.php
+# Fixes typo in wp-settings.php
 
 exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path    => '/usr/local/bin/:/bin/'
 }
-```
-
-### How to run and check it:
-
-1.  **Create the file:**
-    ```bash
-    vi 0-strace_is_your_friend.pp
-    # Paste the puppet code above
-    ```
-
-2.  **Apply the fix:**
-    ```bash
-    puppet apply 0-strace_is_your_friend.pp
-    ```
-
-3.  **Verify:**
-    ```bash
-    curl -I 127.0.0.1
